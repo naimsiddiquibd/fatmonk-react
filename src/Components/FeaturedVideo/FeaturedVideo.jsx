40f91d03ccb8c78ae8f98cfabff597716d03e1bc
Naim Siddiqui <naimsiddiqui@Naims-MacBook-Air.local>
2024-11-06 16:19:41 +0600

    video size updated

--- src/Components/FeaturedVideo/FeaturedVideo.jsx
+++ src/Components/FeaturedVideo/FeaturedVideo.jsx
@@ -5,7 +5,7 @@ const FeaturedVideo = () => {
     const { scrollYProgress } = useScroll();
 
     return (
-        <div className='overflow-hidden w-[100vw] lg:h-[100vw] h-[50vw]'>
+        <div className='overflow-hidden w-[100vw] lg:h-[100vh] h-[50vh]'>
             <img 
                 src="https://res.cloudinary.com/deyd102hk/image/upload/v1730367148/348c5306be17d5c8676b87a7ac46a48c_zdj2nx.gif" 
                 alt="Featured GIF" 
